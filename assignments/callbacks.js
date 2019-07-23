b9d2b0c91bceae64926e335fd59e9ccf58174bf3
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-07-23 17:52:12 -0500

    submitting JSII assignments thus far

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,29 +27,58 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length){
+  console.log(length);
+});
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);//gets the item at the last index
 }
+
+last(items, function(last){
+  console.log(last);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(3, 5, function(sum){
+  console.log(sum);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
-function contains(item, list, cb) {
+multiplyNums(4, 12, function(product){
+  console.log(product);
+});
+
+
+function contains(item, list, cb){
   // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  return list.filter(function(stuff){
+    item == stuff;
+  });
 }
+
+contains("Gum", items, function(test){
+  console.log(`stuff is there = ${test}`)
+}
+
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
+// function removeDuplicates(array, cb) {
+//   // removeDuplicates removes all duplicate values from the given array.
+//   // Pass the duplicate free array to the callback function.
+//   // Do not mutate the original array.
+// }
